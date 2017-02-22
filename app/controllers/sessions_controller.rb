67a670b4a06de2a8a1d52609f9d34ebaf3c90cf6
Nguyen Anh Thi <nguyenanhthi@gogojungle.vn>
2017-02-23 00:29:37 +0700

    Done Milestone 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    
+
   end
-  
+
   def create
     if @user = User.find_by(email: params[:email])
-    if @user.authenticate(params[:password]) # check if password is correct
-      # handle logging in
-      session[:user_id] = @user.id
-      flash[:success] = "Logged in successfully!"
-      redirect_to users_path
-    else
-      # handle "invalid password"
-      flash[:error] = "Invalid password!"
-      render 'new'
-    end
+      if @user.authenticate(params[:password]) # check if password is correct
+        # handle logging in
+        session[:user_id] = @user.id
+        flash[:success] = "Logged in successfully!"
+        redirect_to users_path
+      else
+        # handle "invalid password"
+        flash[:error] = "Invalid password!"
+        render 'new'
+      end
     else
       # handle "user not found"
       flash[:error] = "User not found!"
@@ -23,5 +23,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :sessions
 
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 end
